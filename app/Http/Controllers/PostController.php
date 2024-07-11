44e2014c784b72d001c9135005e0954fe821bccd
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-07-11 10:45:39 +0800

    fix: remove unused line of code

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,7 +35,6 @@ class PostController extends Controller implements HasMiddleware
 
     public function show(Post $post)
     {
-        $postTemp = $post;
         return response()->json($post->load(['tags', 'user', 'comments']), 200);
     }
 
